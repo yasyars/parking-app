d1b04c6654d46a3dfbb5ba9b4bdfb855fbeda920
23520028 <23520028@std.stei.itb.ac.id>
2020-11-10 00:48:51 +0800

    added theme

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import Helper.DbConnection;
 import ViewController.*;
+import javax.swing.*;
 
 public class Main {
 
 
     public static void main (String[] args)  {
         DbConnection.getConnection();
+
+        try {
+            for (UIManager.LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can set the GUI to another look and feel.
+        }
 
         Init initForm = new Init();
         initForm.setVisible(true);
