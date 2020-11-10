7003f2cd11bcb199221c5c03d26ae0d2579989ff
Rizka Ardiantika <23520040@std.stei.itb.ac.id>
2020-11-10 17:45:37 +0700

    Update Kendaraan.java

--- src/Model/Kendaraan.java
+++ src/Model/Kendaraan.java
@@ -5,19 +5,16 @@ import java.util.regex.Pattern;
 public class Kendaraan {
     private String noPlat;
     private String tipe;
-    private User owner;
-    private int isParked;
+    private int owner;
+    
+    public Kendaraan(){}
 
     public String getNoPlat() {
         return noPlat;
     }
 
-    public void setNoPlat(String noPlat) throws Exception {
-        if (isPlatNoValid(noPlat,this.tipe)){
-            this.noPlat = noPlat;
-        }else{
-            throw new Exception("Nomor plat tidak valid");
-        }
+    public void setNoPlat(String noPlat) {
+        this.noPlat = noPlat;
     }
 
     public String getTipe() {
@@ -28,35 +25,12 @@ public class Kendaraan {
         this.tipe = tipe;
     }
 
-    public User getOwner() {
+    public int getOwner() {
         return owner;
     }
 
-    public void setOwner(User owner) {
+    public void setOwner(int owner) {
         this.owner = owner;
     }
-
-    public int getIsParked() {
-        return isParked;
-    }
-
-    public void setIsParked(int isParked) {
-        this.isParked = isParked;
-    }
-
-    public boolean isPlatNoValid(String plat_no, String tipe){
-        String mobilRegex = "^[A-Z]{2}[0-9]{4}[A-Z]{1}$";
-        String motorRegex = "^[A-Z]{2}[0-9]{3}[A-Z]{2}$";
-
-        if (tipe.equals("Mobil")){
-            Pattern p = Pattern.compile(mobilRegex);
-            return p.matcher(plat_no).matches();
-        } else {
-            Pattern p = Pattern.compile(motorRegex);
-            return p.matcher(plat_no).matches();
-        }
-    }
-
-
 }
 
