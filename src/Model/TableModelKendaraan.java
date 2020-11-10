f93059e0d994353968848582716e13c9962d8b0a
Rizka Ardiantika <23520040@std.stei.itb.ac.id>
2020-11-10 17:46:08 +0700

    Update TableModelKendaraan.java

--- src/Model/TableModelKendaraan.java
+++ src/Model/TableModelKendaraan.java
@@ -4,43 +4,38 @@ import java.util.List;
 
 public class TableModelKendaraan extends AbstractTableModel{
 
-    List<Kendaraan> listOfKendaraan;
-
+    List<Kendaraan>listOfKendaraan;
     public TableModelKendaraan(List<Kendaraan> listOfKendaraan){
         this.listOfKendaraan = listOfKendaraan;
     }
-
-    @Override
     public int getRowCount() {
         return this.listOfKendaraan.size();
     }
-
-    @Override
     public int getColumnCount() {
-        return 2;
+        return 3;
     }
 
-    @Override
     public String getColumnName(int column) {
         switch (column){
             case 0:
-                return "Plat No";
+                return "Plat NO";
             case 1:
                 return "Jenis Kendaraan";
+            case 2:
+                return "Id User";
             default:
                 return null;
         }
     }
 
-
-
-    @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
         switch (columnIndex){
             case 0:
                 return listOfKendaraan.get(rowIndex).getNoPlat();
             case 1:
                 return listOfKendaraan.get(rowIndex).getTipe();
+            case 2:
+                return listOfKendaraan.get(rowIndex).getOwner();
             default:
                 return null;
         }
